00bfeac8eba22f4bf7ccb6a2bfe5c9577e8380fd
Adam Gardner <adam.gardner4991@gmail.com>
2020-05-07 16:43:00 +0100

    Initial setup

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+// bootstrap code
+use App\Challenges;
+
+$challenges = new Challenges();
+$challenges->start();
